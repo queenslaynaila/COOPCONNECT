fb54f11881e3a483a71ab437f290030495d5da44
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2023-01-04 02:11:29 +0300

    homepage

---
+++ client/src/components/Home.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import home from '../assets/home2.png'
+import '../styles/home.css'
+export default function Home() {
+  return (
+    <>
+        <div className='container-fluid home bg-image'  >
+            <div class="row">
+            <div class="col-sm-7 pt-5 mt-5 pb-5">
+              <h1  className='find-text'>Find the perfect<br></br>job that you deserve</h1>
+
+              <p className='text mt-2'>Find jobs,Employment and Career Opportunities</p>
+              <div className='search mt-5'>
+                <input type="text" placeholder='job title,keywords or company'></input>
+                <i className='bi bi-search'></i>
+              </div>
+              <p className='mt-2 popular'><bold>Popular Searches:</bold>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Designer ,Developer,Web,IOS,PHP,Senior,Engineer</p>
+            </div>
+            <div class="col-sm-5 pt-5 mt-5 pb-5 mb-3">
+            <img src={home} height="auto" width="400px" alt="Grandma"/>
+            </div>
+            </div><br/>
+
+        </div>
+        <div class="container-xxl py-5">
+            <div class="container">
+                <h1 class="text-center mb-5 wow fadeInUp" data-wow-delay="0.1s">Explore By Category</h1>
+                <div class="row g-4">
+                    <div class="col-lg-3 col-sm-6 border rounded"  >
+                        <a class="cat-item rounded p-4" href="">
+                            <i class="fa fa-3x fa-mail-bulk text-primary mb-4"></i>
+                            <h6 class="mb-3">Marketing</h6>
+                            <p class="mb-0">123 Vacancy</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6  border rounded" >
+                        <a class="cat-item rounded p-4" href="">
+                            <i class="fa fa-3x fa-headset text-primary mb-4"></i>
+                            <h6 class="mb-3">Customer Service</h6>
+                            <p class="mb-0">123 Vacancy</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6  border rounded"  >
+                        <a class="cat-item rounded p-4" href="">
+                            <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
+                            <h6 class="mb-3">Human Resource</h6>
+                            <p class="mb-0">123 Vacancy</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 border rounded"  >
+                        <a class="cat-item rounded p-4" href="">
+                            <i class="fa fa-3x fa-tasks text-primary mb-4"></i>
+                            <h6 class="mb-3">Project Management</h6>
+                            <p class="mb-0">123 Vacancy</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6   border rounded"  >
+                        <a class="cat-item rounded p-4" href="">
+                            <i class="fa fa-3x fa-chart-line text-primary mb-4"></i>
+                            <h6 class="mb-3">Business Development</h6>
+                            <p class="mb-0">123 Vacancy</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6   border rounded"  >
+                        <a class="cat-item rounded p-4" href="">
+                            <i class="fa fa-3x fa-hands-helping text-primary mb-4"></i>
+                            <h6 class="mb-3">Sales & Communication</h6>
+                            <p class="mb-0">123 Vacancy</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 border rounded"  >
+                        <a class="cat-item rounded p-4" href="">
+                            <i class="fa fa-3x fa-book-reader text-primary mb-4"></i>
+                            <h6 class="mb-3">Teaching & Education</h6>
+                            <p class="mb-0">123 Vacancy</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6  border rounded" >
+                        <a class="cat-item rounded p-4" href="">
+                            <i class="fa fa-3x fa-drafting-compass text-primary mb-4"></i>
+                            <h6 class="mb-3">Design & Creative</h6>
+                            <p class="mb-0">123 Vacancy</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
