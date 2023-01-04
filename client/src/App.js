0c9a8e816b69ae4933da21ab3c706241138d407c
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2023-01-05 02:42:33 +0300

    update logins css

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Login from "./components/Login"
+import SignCompanies from "./components/SignCompanies"
+import SignUpJobSeekers from "./components/SignUpJobSeekers"
 function App() {
 
 
   return (
 <>
-<Navbar></Navbar>
-    <Home></Home>
-    <Footer></Footer>
+ <Navbar></Navbar>
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Home></Home>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signupcompanies" element={<SignCompanies/>}/>
+      <Route path="/signuptalents" element={<SignUpJobSeekers/>}/>
+    </Routes>
+  </Router>
+  <Footer></Footer>
 </>
-
   );
 }
 
