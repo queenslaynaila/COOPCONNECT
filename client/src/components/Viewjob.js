e5fc70f694c853a175bade582a07393d789af767
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2023-01-10 11:48:32 +0300

    view a job

--- client/src/components/Viewjob.js
+++ client/src/components/Viewjob.js
@@ -1,16 +1,54 @@
 import React from 'react'
-import ContainerOutline from './reusableComponents/ContainerOutline'
-import JobTitle from './reusableComponents/JobTitle'
-import "../styles/reusableCss/viewJobs.css"
-import AboutCompany from './reusableComponents/AboutCompany'
-import JobDescription from './reusableComponents/JobDescription'
+
 function Viewjob() {
   return (
-    <ContainerOutline>
-      <JobTitle/>
-      <JobDescription/>
-      <AboutCompany/>
-    </ContainerOutline>
+    <div className="container mt-4 mb-4">
+       <div class="card mt-3"  >
+        <div class="card-body">
+            <div className='d-flex flex-row'
+                        style=  {{gap:"15px"}}>
+                <div
+                        style={{backgroundColor:"darkblue",  width:   "70px",  height: "70px", borderRadius: "50%",textAlign:"center",color:"white",lineHeight:"70px",fontSize:"2rem"}} class="circle-singleline mr-auto ">
+                                TS
+                </div>
+                <div>
+                    <p>HR Executives</p>
+                    <p className='fw-bold'  style={{color:"darkblue"}}>Tech Software</p>
+                </div>
+          </div>
+          <div >
+                <div><span className='fw-bold'>Opennings:</span>&nbsp;&nbsp;7</div>
+                <div><span className='fw-bold'>Category:</span>&nbsp;&nbsp;Software Engineering</div>
+                <div><span className='fw-bold'> Salary Range</span>:&nbsp;&nbsp;10,000-20,000</div>
+          </div>
+
+
+       </div>
+       <div class="card-body d-flex flex-row justify-content-between"
+            style=  {{backgroundColor:"#EEEEEE",height:"60px"}}>
+          <div>
+            <p>Posted 16 days ago</p>
+          </div>
+          <div>
+             <button type="button" class="btn btn-primary">Apply Now</button>
+          </div>
+        </div>
+      </div>
+      <div class="card mt-3">
+          <div class="card-body">
+            <h5 style={{color:"darkblue"}}>Job Description</h5>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora totam inventore ex pariatur temporibus!</p>
+            <div><span className='fw-bold'>Positions available:</span>&nbsp;&nbsp;7</div>
+             <div><span className='fw-bold'>Key Skills:</span>&nbsp;&nbsp;Teamwork,Communication</div>
+          </div>
+      </div>
+      <div class="card mt-3">
+          <div class="card-body">
+            <h5 style={{color:"darkblue"}}>About Company</h5>
+            <p>A software development Company</p>
+          </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/move.js
+++ client/src/components/move.js
@@ -1,27 +1,49 @@
 
-<div className='home  pt-4   '>
-        <div className=" d-flex">
-          <div className="p-2 flex-fill bd-highlight my-auto">
-             <div className='welcome-text '>
-                <h1  className='find-text'>Find the perfect<br></br>job that you deserve</h1>
-                <div className='text'>
-                  Find jobs,Employment and Career Opportunities
-                </div>
-             </div>
+   <div className="container mt-4 mb-4">
+   <div class="card mt-3"  >
+    <div class="card-body">
+        <div className='d-flex flex-row'
+                    style=  {{gap:"15px"}}>
+            <div
+                    style={{backgroundColor:"darkblue",  width:   "70px",  height: "70px", borderRadius: "50%",textAlign:"center",color:"white",lineHeight:"70px",fontSize:"2rem"}} class="circle-singleline mr-auto ">
+                            TS
+            </div>
+            <div>
+                <p>HR Executives</p>
+                <p className='fw-bold'  style={{color:"darkblue"}}>Tech Software</p>
+            </div>
+      </div>
+      <div >
+            <div><span className='fw-bold'>Opennings:</span>&nbsp;&nbsp;7</div>
+            <div><span className='fw-bold'>Category:</span>&nbsp;&nbsp;Software Engineering</div>
+            <div><span className='fw-bold'> Salary Range</span>:&nbsp;&nbsp;10,000-20,000</div>
+      </div>
 
 
-
-
-             <div className='searchcontainer'>
-                 <form className='search-bar'>
-                    <input type="text" placeholder='Search by title, keywords or company' name="q"></input>
-                    <button type="submit">Search</button>
-                 </form>
-             </div>
-           
-          </div>
-          <div className="  p-2 flex-fill ">
-             <img src={home} height="auto" width="400px" alt="Grandma"/>
-         </div>
-        </div>
+   </div>
+   <div class="card-body d-flex flex-row justify-content-between"
+        style=  {{backgroundColor:"#EEEEEE",height:"60px"}}>
+      <div>
+        <p>Posted 16 days ago</p>
+      </div>
+      <div>
+         <button type="button" class="btn btn-primary">Apply Now</button>
+      </div>
     </div>
+  </div>
+  <div class="card mt-3">
+      <div class="card-body">
+        <h5 style={{color:"darkblue"}}>Job Description</h5>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempora totam inventore ex pariatur temporibus!</p>
+        <div><span className='fw-bold'>Positions available:</span>&nbsp;&nbsp;7</div>
+         <div><span className='fw-bold'>Key Skills:</span>&nbsp;&nbsp;Teamwork,Communication</div>
+      </div>
+  </div>
+  <div class="card mt-3">
+      <div class="card-body">
+        <h5 style={{color:"darkblue"}}>About Company</h5>
+        <p>A software development Company</p>
+      </div>
+  </div>
+</div>
+)
