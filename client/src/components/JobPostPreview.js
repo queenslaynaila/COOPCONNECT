fcb57fe841baeec6f143306776873932d762c0df
+ <+>
2023-01-10 13:57:24 +0300

    updates

--- client/src/components/JobPostPreview.js
+++ client/src/components/JobPostPreview.js
@@ -1,139 +1,52 @@
-import React, { useState } from "react";
-import "../JobPostPreview.css";
-const JobPostPreview = () => {
-  const [formData, setFormData] = useState({
-    company: "Lencare General Merchants HR",
-    salaryRange: "10,000-50,000",
-    positionsAvailable: "2",
-    location: "Nairobi, Kenya",
-    responsibilities: "2 year experience\nMust know JS",
-    keySkills: "Teamwork\nCommunication\nPHP",
-    experienceInYears: "2 years",
-    seniorityLevel: "Expert",
-    minEducationalQualifications: "Expert",
-  });
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { JobDetailContainer, JobTitle, JobInfo } from './JobPostPreview.css'
 
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
+const JobPostPreview = ({ match }) => {
+  const job = useSelector(state => state.jobs.find(job => job.id === match.params.id))
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Submit the form data to your backend or API here
-  };
+  // Define state variables for handling button clicks
+  const [isEditing, setIsEditing] = useState(false);
 
-  const handleCancel = () => {
-    // Clear the form data and navigate back to the job posts list
-  };
+  if (!job) {
+    return <div>Loading...</div>
+  }
 
-  const handleEdit = () => {
-    // Enable the form fields for editing
-    const inputs = document.querySelectorAll("input, textarea");
-    inputs.forEach((input) => {
-      input.removeAttribute("readonly");
-    });
-  };
+  const handleEditClick = () => {
+    setIsEditing(true);
+    // Add the code for handling the Edit button click here
+  }
+
+  const handleSaveClick = () => {
+    setIsEditing(false);
+    // Add the code for handling the Save button click here
+  }
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+    // Add the code for handling the Cancel button click here
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Company:
-        <input
-          type="text"
-          name="company"
-          value={formData.company}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Salary Range:
-        <input
-          type="text"
-          name="salaryRange"
-          value={formData.salaryRange}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Positions Available:
-        <input
-          type="number"
-          name="positionsAvailable"
-          value={formData.positionsAvailable}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Location:
-        <input
-          type="text"
-          name="location"
-          value={formData.location}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Responsibilities:
-        <textarea
-          name="responsibilities"
-          value={formData.responsibilities}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Key Skills:
-        <textarea
-          name="keySkills"
-          value={formData.keySkills}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Experience in Years:
-        <input
-          type="text"
-          name="experienceInYears"
-          value={formData.experienceInYears}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      Seniority Level:
-      <input
-        type="text"
-        name="seniorityLevel"
-        value={formData.seniorityLevel}
-        onChange={handleChange}
-      />
-      <br />
-      <label>
-        Minimum Educational Qualifications:
-        <input
-          type="text"
-          name="minEducationalQualifications"
-          value={formData.minEducationalQualifications}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <button type="submit">Save Post</button>
-      <button type="button" onClick={handleCancel}>
-        Cancel
-      </button>
-      <button type="button" onClick={handleEdit}>
-        Edit
-      </button>
-    </form>
-  );
-};
+    <div className={JobDetailContainer}>
+      <h2 className={JobTitle}>{job.jobtitle}</h2>
+      <p className={JobInfo}>Responsibilities: {job.responsibilities}</p>
+      <p className={JobInfo}>Key Skills: {job.keyskills}</p>
+      <p className={JobInfo}>Experience in years: {job.experienceinyears}</p>
+      <p className={JobInfo}>Seniority Level: {job.senioritylevel}</p>
+      <p className={JobInfo}>Minimum Educational Qualification: {job.minimaleducationalqualigications}</p>
+      
+      {!isEditing && 
+        <button onClick={handleEditClick}>Edit Post</button>
+      }
+      {isEditing && 
+        <>
+        <button onClick={handleSaveClick}>Save Post</button>
+        <button onClick={handleCancelClick}>Cancel</button>
+        </>
+      }
+    </div>
+  )
+}
 
 export default JobPostPreview;
