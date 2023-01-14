641cbfec4126629ac1a3bd3e05f8ac4e4949cd56
Kazungu Derrick <kazunguderriq@gmail.com>
2023-01-14 20:53:00 +0300

    add jobsapplied page

--- client/src/components/JobsApplied.js
+++ client/src/components/JobsApplied.js
@@ -1,12 +1,11 @@
 import React from "react";
-import '../styles/JobsApplied.css';
+import "../styles/JobsApplied.css";
 
 function JobsApplied() {
   return (
     <div className="main-card">
       <div className="main-card-title">
         <h2>My Applications</h2>
-        
       </div>
       <div className="job-applications-details">
         <p>Title: Software Developer</p>
@@ -14,10 +13,8 @@ function JobsApplied() {
         <p>Posted Date: 20/12/2022</p>
         <p>Category: IT</p>
         <p>Salary: 20-30000</p>
-        <button>View</button>
+        <button className="btn">View</button>
       </div>
-     
-      {/* <div className="lower-section">huhef</div> */}
     </div>
   );
 }
