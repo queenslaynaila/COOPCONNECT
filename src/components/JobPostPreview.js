5d5cac755612ceef26afe47b837df4c7b855261e
+ <+>
2023-01-15 15:16:24 +0300

    job post preview updates

--- src/components/JobPostPreview.js
+++ src/components/JobPostPreview.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 export default function JobPostPreview() {
+  let navigate = useNavigate();
+  const [jobPost, setJobPost] = useState({});
+
+useEffect(() => {
+axios
+.get("/jobPost/:id")
+.then((response) => setJobPost(response.data))
+.catch((error) => console.log(error));
+}, []);
+
+const handleCancel = () => {
+setJobPost({});
+};
+
+const handleSave = () => {
+axios
+.post("/api/jobPost", jobPost)
+.then((response) => {
+console.log(response);
+navigate("/employerdash");
+})
+.catch((error) => console.log(error));
+};
   return (
       <div className='container mt-4 mb-4'>
          <h5 style={{ color: "darkblue" }}>Job Post Preview</h5>
@@ -8,27 +34,28 @@ export default function JobPostPreview() {
             <div class="card-body">
               <div>
                     <div>
-                      <h6 style={{ color: "darkblue" }} className='fw-bold'>Lencare General Merchants HR</h6>
-                      <p>Salary Range:10,000-50,000</p>
-                      <p>Positions Available:2</p><p>Location: nairobi;Kenya</p>
+                    <h6 style={{ color: "darkblue" }} className='fw-bold'>{jobPost.organizationname}</h6>
+                      <p>Salary Range:{jobPost.minsalary}-{jobPost.maxsalary}</p>
+                      <p>Positions Available:{jobPost.positionavailable}</p>
+                      <p>Location: {jobPost.location}</p>
                     </div>
                     <div>
-                       <h6 style={{ color: "darkblue" }}className='fw-bold'>Responsibilities</h6>
-                       <p>2 year experience</p>
-                       <p>Must know JS</p>
+                    <h6 style={{ color: "darkblue" }} className='fw-bold'>Responsibilities</h6>
+                      <p>Experience in Years: {jobPost.experience}</p>
+                      <p>{jobPost.jobresponsibilities}</p>
                     </div>
-                    <div><span style={{ color: "darkblue" }} className='fw-bold'>Key Skills:</span>&nbsp;&nbsp;Teamwork,Communication,php</div>
-                    <div><span style={{ color: "darkblue" }} className='fw-bold'>Experience in Years:</span>&nbsp;&nbsp;2 years</div>
-                    <div><span  style={{ color: "darkblue" }}className='fw-bold'>Seniority Level</span>:&nbsp;&nbsp;Expert</div>
-                    <div><span style={{ color: "darkblue" }} className='fw-bold'>Minimum Educational Qualifications</span>:&nbsp;&nbsp;Expert</div>
+                    <div><span style={{ color: "darkblue" }} className='fw-bold'>Key Skills:</span>&nbsp;&nbsp;{jobPost.keyskills}</div>
+                    <div><span style={{ color: "darkblue" }} className='fw-bold'>Experience in Years:</span>&nbsp;&nbsp;{jobPost.experience}</div>
+                    <div><span  style={{ color: "darkblue" }}className='fw-bold'>Seniority Level</span>:&nbsp;&nbsp;{jobPost.senioritylevel}</div>
+                    <div><span style={{ color: "darkblue" }} className='fw-bold'>Minimum Educational Qualifications</span>:&nbsp;&nbsp;{jobPost.education}</div>
               </div>
             </div>
             <div class="card-body   "
                 style=  {{backgroundColor:"#EEEEEE",height:"60px"}}>
                 <div className="d-flex justify-content-end gap-2">
-                    <button type="button " class="btn btn-primary">EDIT POST</button>
-                  <button type="button" class="btn btn-primary">SAVE POST</button>
-                  <button type="button" class="btn btn-primary">CANCEL</button>
+                    <button type="button " class="btn btn-primary" onClick={() => {navigate("/postajob")}}>EDIT POST</button>
+                  <button type="button" class="btn btn-primary" onClick={handleSave}>SAVE POST</button>
+                  <button type="button" class="btn btn-primary"onClick={handleCancel}>CANCEL</button>
                 </div>
             </div>
          </div>
