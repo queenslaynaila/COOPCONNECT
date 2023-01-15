64a5e0f5e9b83419ed7a73ca05ac85ccd8fc3b8f
+ <+>
2023-01-15 11:43:04 +0300

    survey table updates

--- src/components/SurveyTable.js
+++ src/components/SurveyTable.js
@@ -1,41 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function SurveyTable() {
+  const [surveyData, setSurveyData] = useState([]);
+
+  useEffect(() => {
+    axios.get('/surveys')
+      .then(response => {
+        setSurveyData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <table class="table">
-    <thead>
-       <tr>
+      <thead>
+        <tr>
           <th scope="col">#</th>
-          <th scope="col">SurveyTitle</th>
-          <th scope="col">Total Partcipation</th>
+          <th scope="col">Survey Title</th>
+          <th scope="col">Total Participation</th>
           <th scope="col">Total Passed</th>
           <th scope="col">Total Failed</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>Otto</td>
-        <td>@QUEE o</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Thornton</td>
-        <td>@fat</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        <td>Otto</td>
-      </tr>
-    </tbody>
-  </table>
+        </tr>
+      </thead>
+      <tbody>
+        {surveyData.map((survey, index) => (
+          <tr key={index}>
+            <th scope="row">{index + 1}</th>
+            <td>{survey.title}</td>
+            <td>{survey.total_participation}</td>
+            <td>{survey.total_passed}</td>
+            <td>{survey.total_failed}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
-
