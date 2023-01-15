ef4fa988cdc04c185a147c440c86d1a133e30c03
+ <+>
2023-01-15 11:38:07 +0300

    jobpost table updates

--- src/components/JobpostedTable.js
+++ src/components/JobpostedTable.js
@@ -1,44 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 export default function JobpostedTable() {
+  const [jobData, setJobData] = useState({});
+
+  useEffect(() => {
+    axios.get('/jobs')
+      .then(response => {
+        setJobData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <table class="table">
-    <thead>
-       <tr>
+      <thead>
+        <tr>
           <th scope="col">#</th>
           <th scope="col">Job Title</th>
           <th scope="col">Job Category</th>
           <th scope="col">Total Applicants</th>
-          <th scope="col">Acepted</th>
+          <th scope="col">Accepted</th>
           <th scope="col">Rejected</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td >Larry the Bird</td>
-        <td>@twitter</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        <td>@mdo</td>
-      </tr>
-    </tbody>
-  </table>
+        </tr>
+      </thead>
+      <tbody>
+        {jobData.job_posts ? jobData.job_posts.map((job, index) => (
+          <tr key={index}>
+            <th scope="row">{index + 1}</th>
+            <td>{job.job_title}</td>
+            <td>{job.job_category}</td>
+            <td>{job.total_applicants}</td>
+            <td>{job.accepted}</td>
+            <td>{job.rejected}</td>
+          </tr>
+        )) : null}
+      </tbody>
+    </table>
   )
 }
