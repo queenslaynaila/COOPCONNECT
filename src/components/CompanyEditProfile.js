4145fa7a14b55f567bffe32c7a77c394566d53a1
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2023-01-17 09:04:11 +0300

    enable seeker edit profile

--- src/components/CompanyEditProfile.js
+++ src/components/CompanyEditProfile.js
@@ -1,11 +1,35 @@
 import React, { useState } from "react";
 import "../styles/CompanyEditProfile.css"
 import { useNavigate } from "react-router-dom";
-function CompanyEditProfile() {
+function CompanyEditProfile({employer}) {
   let navigate = useNavigate();
+  const [name,Setname] =useState(employer.name)
+  const [companytype,Setcompanytype] = useState("")
+  const [websiteurl,Setwebsiteurl] = useState('')
+  const [description,Setdescription] = useState('')
+  const [firstname, SetFirstName] = useState(employer.firstname);
+  const [secondname, SetSecondName] = useState(employer.secondname);
+  const [designation,Setdesignation] = useState('')
+
+  if (name === null || firstname===null || secondname===null )  {
+    SetFirstName(employer.firstname)
+     Setname(employer.name)
+     SetSecondName(employer.secondname)
+  }
   function handleUpdate(e){
     e.preventDefault()
+    fetch(`/employers/${employer.id}`, {
+      method: "PATCH",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({firstname,secondname,name,companytype,websiteurl,description,designation})
+    }).then(res => {
+      if(res.ok){
+          alert("Post was succesfull")
+      }
+    });
   }
+
+
   return (
     <div className="container mt-4 mb-4">
        <h4 style={{color:"darkblue"}}>Profile Edit Mode</h4>
@@ -18,21 +42,29 @@ function CompanyEditProfile() {
           <div class="card-body">
              <form id="bookingForm" action="#" method="" class="needs-validation" novalidate autocomplete="off">
                 <div class="form-group">
-                    <input type="text" class="form-control" id="inputName" name="name" placeholder="Organization Name"  />
+                    <input type="text" class="form-control" id="inputName" name="name" placeholder="Organization Name" value={name}  onChange={(e)=>{
+                      Setname(e.target.value)
+                    }}/>
                 </div>
                 <div class="row">
 
                     <div class="col">
                         <div class="form-group">
-                                      <input type="text" class="form-control" id="inputEmail" name="email" placeholder="companytype"  />
+                                      <input type="text" class="form-control" id="companytype" name="companytype" placeholder="companytype" onChange={(e)=>{
+                                        Setcompanytype(e.target.value)
+                                      }} value={companytype}  />
                         </div>
                     </div>
                </div>
               <div class="form-group">
-                <input type="text" class="form-control" id="inputPhone" name="phone" placeholder="Website url" />
+                <input type="string" class="form-control form-control-lg mb-2" id="iurl" name="iurl" placeholder="Website url" value={websiteurl} onChange={(e)=>{
+                  Setwebsiteurl(e.target.value)
+                }}/>
               </div>
               <div class="form-group">
-                <textarea class="form-control" name="remark" id="textAreaRemark" rows="2" placeholder="Describe your company..."></textarea>
+                <textarea class="form-control" name="remark" id="textAreaRemark" rows="2" placeholder="Describe your company..." value={description} onChange={(e)=>{
+                  Setdescription(e.target.value)
+                }}></textarea>
              </div>
             </form>
           </div>
@@ -44,25 +76,31 @@ function CompanyEditProfile() {
                 <div class="row">
                     <div class="col">
                         <div class="form-group">
-                                  <input type="text" class="form-control" id="inputEmail" name="email" placeholder="First Name"  />
+                                  <input type="text" class="form-control" id="inputEmail" name="email" placeholder="First Name" value={firstname} onChange={(e)=>{
+                                    SetFirstName(e.target.value)
+                                  }} />
                         </div>
                     </div>
                     <div class="col">
                         <div class="form-group">
-                                      <input type="text" class="form-control" id="inputEmail" name="email" placeholder="Last Name"  />
+                                      <input type="text" class="form-control" id="sname" name="sname" placeholder="Last Name" value={secondname} onChange={(e)=>{
+                                        SetSecondName(e.target.value)
+                                      }} />
                         </div>
                     </div>
                </div>
                <div class="row">
                     <div class="col">
                         <div class="form-group">
-                                  <input type="text" class="form-control" id="inputEmail" name="email" placeholder="Designation"  />
+                                  <input type="text" class="form-control" id="des" name="des" placeholder="Designation"  value={designation} onChange={(e)=>{
+                                    Setdesignation(e.target.value)
+                                  }} />
                         </div>
                     </div>
 
                </div>
                <div className="d-flex justify-content-end">
-               <button onClick={(e)=>handleUpdate} type="button" class="btn btn-primary" >UPDATE</button>
+               <button onClick={(e)=>handleUpdate(e)} type="button" class="btn btn-primary" >UPDATE</button>
                </div>
 
             </form>
