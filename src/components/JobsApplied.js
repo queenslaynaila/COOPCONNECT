e9aa8b5663e670e63510ad1840495e2d0277dfe5
+ <+>
2023-01-18 01:49:30 +0300

    updates

--- src/components/JobsApplied.js
+++ src/components/JobsApplied.js
@@ -3,7 +3,10 @@ import { useEffect ,useState,u} from "react";
 import { useNavigate } from "react-router";
 import Appliedjobcard from "./Appliedjobcard";
 function JobsApplied({seeker}) {
-  const [jobs,setJobs] = useState([])
+const [jobs,setJobs] = useState([])
+const [search,setsearch] = useState(" ")
+const [maximumsalary,setMaximumSalary] = useState(null)
+const [minsalary,setMinSalary ] = useState(null)
     let navigate = useNavigate()
 
  useEffect(() => {
@@ -14,11 +17,44 @@ function JobsApplied({seeker}) {
   });
 }, []);
 console.log(jobs)
+// const handleSearch = (event) => {
+//   setSearchValue(event.target.value);
+// };
+
+// const handleMinSalary = (event) => {
+//   setMinSalary(event.target.value);
+// }
+
+// const handleMaxSalary = (event) => {
+//   setMaxSalary(event.target.value);
+// }
+
+// const filteredJobs = jobs.filter((job) => 
+//   job.title.toLowerCase().includes(searchValue.toLowerCase()) &&
+//   (job.salary >= minSalary) &&
+//   (job.salary <= maxSalary) 
+// );
+
+function handleSearch(){
+    const filterSEarch = jobs.filter((job)=>{return job.jobtitle.toLowerCase().includes(search.toLowerCase()) })
+    setJobs(filterSEarch)
+
+}
+function handleWageFilter(){
+    const filterredjobs = jobs.filter((job)=>{return job.minsalary > minsalary && job.maximumsalary < maximumsalary})
+     setJobs(filterredjobs)
+     console.log(jobs)
+
+}
+function handleChange(event){
+    setsearch(event.target.value)
+    console.log(search)
+   }
   return (
     <div class="container-sm mt-4 mb-4">
-       <h2 className="mt-2" style={{color:"darkblue"}}>JOB APPLICATIONS </h2>
+       <h2 className="mt-2" style={{color:"darkblue"}}>JOB APPLICATIONS{" "} </h2>
        <div class="row mt-5 mb-5">
-            <p  className="dash" onClick={() => {navigate("/talentdashboard")}} style={{cursor:"pointer"}}> <i class="bi bi-arrow-left"></i> Go to Dashboard
+            <p  className="dash" onClick={() => {navigate("/talentdashboard")}} style={{cursor:"pointer"}}>{" "} <i class="bi bi-arrow-left"></i> Go to Dashboard
             </p>
             <aside class="col-md-3">
             <div class="card">
@@ -28,18 +64,18 @@ console.log(jobs)
                             <h6 class="title">Salary range </h6>
                     </header>
                     <div class="filter-content collapse show" id="collapse_3"  >
-                        <div class="card-body">
+                    <div class="card-body">
                             <div class="form-row">
                             <div class="form-group col-md-6">
                             <label>Min</label>
-                            <input  class="form-control" placeholder="0" type="number"></input>
+                            <input onChange={(e)=>setMinSalary(e.target.value)} class="form-control" placeholder="0" type="number"></input>
                             </div>
                             <div class="form-group text-right col-md-6">
                             <label>Max</label>
-                            <input class="form-control"    placeholder="1,0000" type="number"></input>
+                            <input class="form-control" onChange={(e)=>setMaximumSalary(e.target.value)}   placeholder="1,0000" type="number"></input>
                             </div>
                             </div>
-                            <button   class="btn btn-block btn-primary">Apply</button>
+                            <button onClick={handleWageFilter} class="btn btn-block btn-primary">Apply</button>
                         </div>
                     </div>
                 </article>
@@ -48,21 +84,20 @@ console.log(jobs)
             </div>
         </aside>
         <main class="col-md-9">
-            <header>
+        <header>
                 <div class="form-inline">
                 <div class="input-group">
-	                <input   id="search-input" type="search" class="form-control" placeholder="Search a job, company..."></input>
-                    <button o   id="search-button" type="button" class="btn btn-primary">
-                    <i     class="fa fa-search"></i>
+	                <input  onChange={handleChange}  id="search-input" type="search" class="form-control" placeholder="Search a job, company..."></input>
+                    <button onClick={handleSearch}  id="search-button" type="button" class="btn btn-primary">
+                    <i  onClick={(e)=>handleSearch}  class="fa fa-search"></i>
   	                </button>
                 </div>
                 </div>
             </header>
 
-
-            {jobs.map(job=> <Appliedjobcard key={job.id} job={job}/>)}
-
-
+            {jobs.map((job) => (
+           <Appliedjobcard key={job.id} job={job} />))}
+            
         </main>
       </div>
 
@@ -75,5 +110,4 @@ console.log(jobs)
 
   );
 }
-
 export default JobsApplied;
